86b3333fa5a1a2cef990044a6bd8d17468bd9da8
nocturnal404 <egiehermawan926@gmail.com>
2025-06-17 22:47:32 +0700

    sign in title

--- src/app/(full-width-pages)/(auth)/signin/page.tsx
+++ src/app/(full-width-pages)/(auth)/signin/page.tsx
@@ -3,8 +3,8 @@ import HelloFetcher from "@/components/HelloFetcher";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Next.js SignIn Page | TailAdmin - Next.js Dashboard Template",
-  description: "This is Next.js Signin Page TailAdmin Dashboard Template",
+  title: "Cashlez Promo | Sign-In",
+  description: "Cashlez Promo | Sign-In",
 };
 
 export default function SignIn() {
